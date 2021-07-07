762bf356c11719dff2ddd1ad5d443e4c3ad1f54b
LukasKratochvila <kratochvila.lukas@gmail.com>
2021-07-07 10:58:22 +0200

    LeNet
    Channel parameter

--- networks/main.py
+++ networks/main.py
@@ -2,7 +2,7 @@ from .my_LeNet import MY_LeNet, MY_LeNetBN
 from .my_LeNet_HRes import MY_LeNet as MY_LeNet_HRes, MY_LeNet_Autoencoder
 
 def implemented_networks():
-    return ('my_LeNet', 'my_LeNetBN', 'my_LeNet_Hres')
+    return ('mnist_LeNet','my_LeNet', 'my_LeNetBN', 'my_LeNet_Hres')
 
 def build_network(net_name):
     """Builds the neural network."""
@@ -10,6 +10,9 @@ def build_network(net_name):
     assert net_name in implemented_networks()
 
     net = None
+    if net_name == 'mnist_LeNet':
+        net = MY_LeNetBN(in_channels=1, in_size=28)
+        
     if net_name == 'my_LeNet':
         net = MY_LeNet()
         
--- networks/my_LeNet.py
+++ networks/my_LeNet.py
@@ -15,10 +15,11 @@ from base.base_net import BaseNet
 
 class MY_LeNetBN(BaseNet):
 
-    def __init__(self, rep_dim: int=3, in_channels: int=3):
+    def __init__(self, rep_dim: int=1000, in_channels: int=3, in_size=32):
         super().__init__()
 
         self.rep_dim = rep_dim#1000#128
+        self.out_size = int(in_size/8)
         self.pool = nn.MaxPool2d(2, 2)
 
         self.conv1 = nn.Conv2d(in_channels, 32, 5, bias=True, padding=2)
@@ -27,7 +28,7 @@ class MY_LeNetBN(BaseNet):
         self.bn2d2 = nn.BatchNorm2d(64, eps=1e-04, affine=False)
         self.conv3 = nn.Conv2d(64, 128, 5, bias=True, padding=2)
         self.bn2d3 = nn.BatchNorm2d(128, eps=1e-04, affine=False)
-        self.fc1 = nn.Linear(128 * 4 * 4, self.rep_dim, bias=True) # 4 * 4
+        self.fc1 = nn.Linear(128 * self.out_size * self.out_size, self.rep_dim, bias=True) # 4 * 4
 
         nn.init.xavier_normal_(self.conv1.weight, gain=1.0)
         nn.init.xavier_normal_(self.conv2.weight, gain=1.0)
@@ -50,18 +51,20 @@ class MY_LeNet(BaseNet):
     def __init__(self):
         super().__init__()
 
-        self.rep_dim = 1000#128
+        self.rep_dim = 128#128
         self.pool = nn.MaxPool2d(2, 2)
 
         self.conv1 = nn.Conv2d(3, 32, 5, bias=True, padding=2)
         self.conv2 = nn.Conv2d(32, 64, 5, bias=True, padding=2)
         self.conv3 = nn.Conv2d(64, 128, 5, bias=True, padding=2)
-        self.fc1 = nn.Linear(128 * 4 * 4, self.rep_dim, bias=True) # 20 * 15
+        self.conv4 = nn.Conv2d(128, 256, 5, bias=True, padding=2)
+        self.fc1 = nn.Linear(256 * 2 * 2, self.rep_dim, bias=True) # 20 * 15
 
         #self.soft = nn.Softmax(dim=1)
         nn.init.xavier_normal_(self.conv1.weight, gain=1.0)
         nn.init.xavier_normal_(self.conv2.weight, gain=1.0)
         nn.init.xavier_normal_(self.conv3.weight, gain=1.0)
+        nn.init.xavier_normal_(self.conv4.weight, gain=1.0)
         nn.init.xavier_normal_(self.fc1.weight, gain=1.0)
 
     def forward(self, x):
@@ -70,6 +73,8 @@ class MY_LeNet(BaseNet):
         x = self.conv2(x)
         x = self.pool(F.leaky_relu(x))
         x = self.conv3(x)
+        x = self.pool(F.leaky_relu(x))
+        x = self.conv4(x)
         x = self.pool(F.leaky_relu(x))
         x = x.view(x.size(0), -1)
         x = self.fc1(x)
